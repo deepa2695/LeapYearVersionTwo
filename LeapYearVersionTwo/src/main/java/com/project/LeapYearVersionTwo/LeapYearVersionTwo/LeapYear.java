d89ab566e9753bb93d71c74b8793b1f045297f87
deepa2695 <deepamano2695@gmail.com>
2021-09-17 21:45:07 -0700

    Refactored the class for better readability

--- LeapYearVersionTwo/src/main/java/com/project/LeapYearVersionTwo/LeapYearVersionTwo/LeapYear.java
+++ LeapYearVersionTwo/src/main/java/com/project/LeapYearVersionTwo/LeapYearVersionTwo/LeapYear.java
@@ -1,8 +1,8 @@
 package com.project.LeapYearVersionTwo.LeapYearVersionTwo;
 
 public class LeapYear {
-	public boolean isLeapYear(int i) {
-		if (i % 400 == 0 || (i % 100 != 0 && i % 4 == 0)) {
+	public boolean isLeapYear(int year) {
+		if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
 			return true;
 		}else{
 			return false;
